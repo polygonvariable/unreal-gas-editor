24baacd7512d586e9b6bad09a42252857dc0be72
Ayush Paul Tirkey <ayushpaultirkey2001@gmail.com>
2024-12-01 19:36:38 +0530

    Update #0
    > file read, write function created.
    > regex match, matches, replaceAll, replaceFirstMatch, test functions created.
    > editor utility created.

--- Source/GASEditor/Private/IOLibrary.cpp
+++ Source/GASEditor/Private/IOLibrary.cpp
@@ -2,4 +2,90 @@
 
 
 #include "IOLibrary.h"
+#include "Misc/FileHelper.h"
+#include <regex>
 
+FString UIOLibrary::ReadFile(const FString& FilePath)
+{
+	FString fileContent;
+	FFileHelper::LoadFileToString(fileContent, *FilePath);
+	return fileContent;
+}
+
+bool UIOLibrary::WriteFile(const FString& FilePath, const FString& Content)
+{
+	return FFileHelper::SaveStringToFile(Content, *FilePath);
+}
+
+FString UIOLibrary::GetProjectName()
+{
+	return FString(FApp::GetProjectName());
+}
+
+bool UIOLibrary::RegexTest(const FString& TestString, const FString& Pattern)
+{
+	FRegexPattern regexPattern(Pattern);
+	FRegexMatcher regexMatcher(regexPattern, TestString);
+	return regexMatcher.FindNext();
+}
+
+FString UIOLibrary::RegexMatch(const FString& TestString, const FString& Pattern)
+{
+	FRegexPattern regexPattern(Pattern);
+	FRegexMatcher regexMatcher(regexPattern, TestString);
+
+	if (regexMatcher.FindNext())
+	{
+		return regexMatcher.GetCaptureGroup(0);
+	}
+
+	return FString();
+}
+
+TArray<FString> UIOLibrary::RegexMatches(const FString& TestString, const FString& Pattern)
+{
+    TArray<FString> matches;
+
+    FRegexPattern regexPattern(Pattern);
+    FRegexMatcher regexMatcher(regexPattern, TestString);
+
+    while (regexMatcher.FindNext())
+    {
+        int32 index = 0;
+        FString match;
+
+        while (!(match = regexMatcher.GetCaptureGroup(index)).IsEmpty())
+        {
+			matches.Add(match);
+            ++index;
+        }
+    }
+
+    return matches;
+}
+
+FString UIOLibrary::RegexReplaceAll(const FString& TestString, const FString& Pattern, const FString& ReplaceString)
+{
+	TArray<FString> matches = RegexMatches(TestString, Pattern);
+
+	FString result = TestString;
+
+	for (const FString& match : matches)
+	{
+		result = result.Replace(*match, *ReplaceString);
+	}
+
+	return result;
+}
+
+FString UIOLibrary::ReplaceFirstMatch(FString SourceString, const FString& SearchString, const FString& ReplaceString)
+{
+	int32 index = SourceString.Find(SearchString);
+
+	if (index != INDEX_NONE)
+	{
+		SourceString = SourceString.Left(index) + ReplaceString + SourceString.RightChop(index + SearchString.Len());
+	}
+
+	return SourceString;
+}
--- Source/GASEditor/Public/IOLibrary.h
+++ Source/GASEditor/Public/IOLibrary.h
@@ -13,5 +13,29 @@ UCLASS()
 class GASEDITOR_API UIOLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
+	UFUNCTION(BlueprintCallable)
+	static FString ReadFile(const FString& FilePath);
+
+	UFUNCTION(BlueprintCallable)
+	static bool WriteFile(const FString& FilePath, const FString& Content);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	static FString GetProjectName();
+
+	UFUNCTION(BlueprintCallable)
+	static bool RegexTest(const FString& TestString, const FString& Pattern);
+
+	UFUNCTION(BlueprintCallable)
+	static FString RegexMatch(const FString& TestString, const FString& Pattern);
+
+	UFUNCTION(BlueprintCallable)
+	static TArray<FString> RegexMatches(const FString& TestString, const FString& Pattern);
+
+	UFUNCTION(BlueprintCallable)
+	static FString RegexReplaceAll(const FString& TestString, const FString& Pattern, const FString& ReplaceString);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	static FString ReplaceFirstMatch(FString SourceString, const FString& SearchString, const FString& ReplaceString);
+
 };
