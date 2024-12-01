2755eed3dcded6f422674dc59d1b3a8b56e98ad6
Ayush Paul Tirkey <ayushpaultirkey2001@gmail.com>
2024-12-01 18:28:48 +0530

    Initial commit
    > GAS Attribute editor to add and remove attribute set at runtime.

---
+++ Source/GASEditor/GASEditor.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GASEditor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GASEditor, "GASEditor" );
---
+++ Source/GASEditor/Public/IOLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "IOLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASEDITOR_API UIOLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
